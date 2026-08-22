21335a61a881d029a6a858badf0ca00840f04eb4
qfmeng <qfmeng@iflytek.com>
2026-08-22 11:53:15 +0800

    SP_A 4b5bb21e-d676-4092-93b2-9c964377f5f4_1787370794886

---
+++ idcard_lib/build/generated/source/buildConfig/release/me/blankm/idcardlib/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.blankm.idcardlib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "me.blankm.idcardlib";
+  public static final String BUILD_TYPE = "release";
+}
